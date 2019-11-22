a9b05744cc3344b41e508e757ea7a724a650d494
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2019-11-22 19:46:45 +0800

    Update v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh

--- v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
+++ v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
@@ -634,9 +634,9 @@ install_v2ray_ws_tls()
 ##安装nginx
     wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz
     tar -zxf openssl-1.1.1a.tar.gz
-    wget https://nginx.org/download/nginx-1.17.5.tar.gz
-    tar -zxf nginx-1.17.5.tar.gz
-    cd nginx-1.17.5
+    wget https://nginx.org/download/nginx-1.17.6.tar.gz
+    tar -zxf nginx-1.17.6.tar.gz
+    cd nginx-1.17.6
     ./configure --prefix=/etc/nginx --with-openssl=../openssl-1.1.1a --with-openssl-opt="enable-tls1_3 enable-tls1_2 enable-tls1 enable-ssl enable-ssl2 enable-ssl3 enable-ec_nistp_64_gcc_128 shared threads zlib-dynamic sctp" --with-mail=dynamic --with-mail_ssl_module --with-stream=dynamic --with-stream_ssl_module --with-stream_realip_module --with-stream_geoip_module=dynamic --with-stream_ssl_preread_module --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-pcre --with-libatomic --with-compat --with-cpp_test_module --with-google_perftools_module --with-file-aio --with-threads --with-poll_module --with-select_module
     make
     make install
@@ -647,11 +647,11 @@ install_v2ray_ws_tls()
 
 ##清除垃圾
     cd ..
-    rm -rf nginx-1.17.5
+    rm -rf nginx-1.17.6
     rm -rf *bbr.*
     rm -rf bbr2.s*
-    rm -rf nginx-1.17.5.tar.gz
-    rm -rf nginx-1.17.5.tar.gz*
+    rm -rf nginx-1.17.6.tar.gz
+    rm -rf nginx-1.17.6.tar.gz*
     rm -rf openssl-1.1.1a.tar.gz
     rm -rf openssl-1.1.1a.tar.gz*
     rm -rf openssl-1.1.1a
