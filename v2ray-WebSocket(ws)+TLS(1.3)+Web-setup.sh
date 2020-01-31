770d3c800102041dc33202e9a1379b8641ebabf6
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2020-01-31 18:03:28 +0800

    Update v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh

--- v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
+++ v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
@@ -4,19 +4,19 @@
 #定义几个颜色
 tyblue()                           #天依蓝
 {
-    echo -e "\033[36;1m $1 \033[0m"
+    echo -e "\033[36;1m ${1}\033[0m"
 }
 green()                            #水鸭青
 {
-    echo -e "\033[32;1m $1 \033[0m"
+    echo -e "\033[32;1m ${1}\033[0m"
 }
 yellow()                           #鸭屎黄
 {
-    echo -e "\033[33;1m $1 \033[0m"
+    echo -e "\033[33;1m ${1}\033[0m"
 }
 red()                              #姨妈红
 {
-    echo -e "\033[31;1m $1 \033[0m"
+    echo -e "\033[31;1m ${1}\033[0m"
 }
 
 
@@ -222,45 +222,23 @@ server {
     server_name $domain;
     return 301 https://\$host\$request_uri;
 }
-EOF
-    if [ $tlsVersion -eq 2 ]; then
-cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
 server {
     listen 443 ssl http2 default_server;
     listen [::]:443 ssl http2 default_server;
     ssl_certificate       /etc/nginx/certs/$domain.cer;
     ssl_certificate_key   /etc/nginx/certs/$domain.key;
-    ssl_protocols         TLSv1.3;
-    return 301 https://$domain\$request_uri;
-}
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name $domain;
-    ssl_certificate       /etc/nginx/certs/$domain.cer;
-    ssl_certificate_key   /etc/nginx/certs/$domain.key;
-    ssl_protocols         TLSv1.3;
-    root /etc/nginx/html/$domain;
-    index index.html;
-    location /$path {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:$port;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-    }
-}
+EOF
+    if [ $tlsVersion -eq 1 ]; then
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
+    ssl_protocols         TLSv1.3 TLSv1.2;
+    ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305;
 EOF
     else
 cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
-server {
-    listen 443 ssl http2 default_server;
-    listen [::]:443 ssl http2 default_server;
-    ssl_certificate       /etc/nginx/certs/$domain.cer;
-    ssl_certificate_key   /etc/nginx/certs/$domain.key;
-    ssl_protocols         TLSv1.3 TLSv1.2;
-    ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305;
+    ssl_protocols         TLSv1.3;
+EOF
+    fi
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
     return 301 https://$domain\$request_uri;
 }
 server {
@@ -269,11 +247,22 @@ server {
     server_name $domain;
     ssl_certificate       /etc/nginx/certs/$domain.cer;
     ssl_certificate_key   /etc/nginx/certs/$domain.key;
+EOF
+    if [ $tlsVersion -eq 1 ]; then
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
     ssl_protocols         TLSv1.3 TLSv1.2;
     ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305;
-    root /etc/nginx/html/$domain;
-    index index.html;
-    location /$path {
+EOF
+    else
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
+    ssl_protocols         TLSv1.3;
+EOF
+    fi
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
+    location / {
+        return 403;
+    }
+    location $path {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:$port;
         proxy_http_version 1.1;
@@ -283,7 +272,6 @@ server {
     }
 }
 EOF
-    fi
     if [ $domainconfig -eq 1 ]; then
         sed -i "s/server_name $domain/& www.$domain/" /etc/nginx/conf.d/v2ray.conf
     fi
@@ -296,8 +284,6 @@ new_tls()
     configtls_part
     old_domain=$(grep -m 1 "server_name" /etc/nginx/conf.d/v2ray.conf)
     old_domain=${old_domain%';'*}
-    case "$tlsVersion" in
-        2)
 cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
 server {
     listen 443 ssl http2;
@@ -305,33 +291,22 @@ server {
     server_name $domain;
     ssl_certificate       /etc/nginx/certs/$domain.cer;
     ssl_certificate_key   /etc/nginx/certs/$domain.key;
-    ssl_protocols         TLSv1.3;
-    root /etc/nginx/html/$domain;
-    index index.html;
-    location /$path {
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:$port;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
-    }
-}
 EOF
-            ;;
-        1)
+    if [ $tlsVersion -eq 1 ]; then
 cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name $domain;
-    ssl_certificate       /etc/nginx/certs/$domain.cer;
-    ssl_certificate_key   /etc/nginx/certs/$domain.key;
     ssl_protocols         TLSv1.3 TLSv1.2;
     ssl_ciphers           ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305;
-    root /etc/nginx/html/$domain;
-    index index.html;
-    location /$path {
+EOF
+    else
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
+    ssl_protocols         TLSv1.3;
+EOF
+    fi
+cat >> /etc/nginx/conf.d/v2ray.conf<<EOF
+    location / {
+        return 403;
+    }
+    location $path {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:$port;
         proxy_http_version 1.1;
@@ -341,8 +316,6 @@ server {
     }
 }
 EOF
-            ;;
-    esac
     if [ $domainconfig -eq 1 ]; then
         sed -i "0,/$old_domain/s//$old_domain $domain www.$domain/" /etc/nginx/conf.d/v2ray.conf
         sed -i "s/server_name $domain/& www.$domain/" /etc/nginx/conf.d/v2ray.conf
@@ -391,6 +364,7 @@ updateSystem()
     case "$updateconfig" in
         1)
             do-release-upgrade
+            sed -i 's/Prompt=lts/Prompt=normal/' /etc/update-manager/release-upgrades
             do-release-upgrade -d
             ;;
         2)
@@ -471,7 +445,7 @@ remove_v2ray_nginx()
 {
     /etc/nginx/sbin/nginx -s stop
     service v2ray stop
-    service v2ray disable
+    #service v2ray disable
     rm -rf /usr/bin/v2ray 
     rm -rf /etc/v2ray
     rm -rf /etc/nginx
@@ -504,18 +478,26 @@ install_bbr()
     done
     case "$bbrconfig" in
         1)
-            tyblue "****即将安装bbr加速，安装完成后可能会重启，若重启，请再次运行此脚本完成剩余安装****"
-            yellow "按回车键以继续。。。。"
-            read rubbish
-            rm -rf bbr.sh
-            if ! wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh ; then
-                red    "获取bbr脚本失败"
-                red    "你的服务器貌似没联网，或不支持ipv4"
-                yellow "按回车键继续或者按ctrl+c终止"
-                read rubbish
+            if ! grep -q "#This file has been edited by v2ray-WebSocket-TLS-Web-setup-script" /etc/sysctl.conf ; then
+                echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+                echo '#This file has been edited by v2ray-WebSocket-TLS-Web-setup-script' >> /etc/sysctl.conf
             fi
-            chmod +x bbr.sh
-            ./bbr.sh
+            if ! sysctl -p | sysctl -a | grep "net.ipv4.tcp_congestion_control" | grep -q "bbr" ; then
+                tyblue "****即将安装bbr加速，安装完成后可能会重启，若重启，请再次运行此脚本完成剩余安装****"
+                yellow "按回车键以继续。。。。"
+                read rubbish
+                rm -rf bbr.sh
+                if ! wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh ; then
+                    red    "获取bbr脚本失败"
+                    red    "你的服务器貌似没联网，或不支持ipv4"
+                    yellow "按回车键继续或者按ctrl+c终止"
+                    read rubbish
+                fi
+                chmod +x bbr.sh
+                ./bbr.sh
+            else
+                green "********************bbr已安装********************"
+            fi
             ;;
         2)
             tyblue "*********************即将安装bbr2加速，安装完成后服务器将会重启*********************"
@@ -606,22 +588,6 @@ get_certs()
 }
 
 
-##下载网站模板，用于伪装
-get_web()
-{
-    rm -rf /etc/nginx/html/$domain
-    mkdir /etc/nginx/html/$domain
-    if ! wget -P /etc/nginx/html/$domain https://github.com/kirin10000/v2ray-WebSocket-TLS-Web-setup-script/raw/master/Website-Template.zip ; then
-        red    "获取网站模板失败"
-        red    "你的服务器貌似没联网，或不支持ipv4"
-        yellow "按回车键继续或者按ctrl+c终止"
-        read rubbish
-    fi
-    unzip -q -d /etc/nginx/html/$domain /etc/nginx/html/$domain/*.zip
-    rm -rf /etc/nginx/html/$domain/*.zip
-}
-
-
 #安装程序主体
 install_v2ray_ws_tls()
 {
@@ -689,77 +655,54 @@ install_v2ray_ws_tls()
 
 
 ##获取端口、id和path
-    path=$(cat /dev/urandom | head -c 8 | md5sum | head -c 6)               ##获取随机值作为path
-    port=`grep port /etc/v2ray/config.json`
-    port=${port##*' '}
-    port=${port%%,*}
-    v2id=`grep id /etc/v2ray/config.json`
-    v2id=${v2id#*:}
-##获取端口、id和path完成
-
-
-    configtls                                                              ##配置nginx
-    get_web
-
-
+    get_info
+##配置nginx
+    configtls
 ##配置v2ray文件
-    sed -i 's/"protocol"/"listen": "127.0.0.1",\n    &/' /etc/v2ray/config.json
-    sed -i 's/"alterId": 64/"alterId": 0/' /etc/v2ray/config.json
-    hang=`sed -n '/"outbounds"/=' /etc/v2ray/config.json`
-    hang=$(($hang+1))
-    hanglast=`sed -n '$=' /etc/v2ray/config.json`
-    sed -i "${hang},${hanglast}d" /etc/v2ray/config.json
-    echo '    "protocol": "freedom",' >> /etc/v2ray/config.json
-    echo '    "settings": {}' >> /etc/v2ray/config.json
-    echo '  }]' >> /etc/v2ray/config.json
-    echo '}' >> /etc/v2ray/config.json
-    sed -i 's#}],#,"streamSettings":{"network"#' /etc/v2ray/config.json
-    sed -i 's#"network"#&:"ws","wsSettings":{"pa#' /etc/v2ray/config.json
-    sed -i s#gs\":{\"pa#\&th\":\"/$path\"}}}],# /etc/v2ray/config.json
-##配置v2ray文件完成
+    config_v2ray_vmess
 
 
     service v2ray restart
     /etc/nginx/sbin/nginx
     case "$domainconfig" in
         1)
-            clear
             tyblue "*************安装完成*************"
             tyblue "地址：www.${domain}或${domain}"
             tyblue "端口：443"
             tyblue "ID：${v2id}"
             tyblue "额外ID：0"
-            tyblue "加密方式：任意(推荐：none)"
+            tyblue "加密方式：一般情况推荐none;若使用了cdn，推荐auto"
             tyblue "传输协议：ws"
             tyblue "伪装类型：none"
             tyblue "伪装域名：空"
-            tyblue "路径：/${path}"
+            tyblue "路径：${path}"
             tyblue "底层传输安全：tls"
             tyblue "**********************************"
             yellow "注意事项：如重新启动服务器，请执行/etc/nginx/sbin/nginx"
+            yellow "          或运行脚本，选择重启服务选项"
             echo
-            tyblue "脚本最后更新时间：2020.1.21"
+            tyblue "脚本最后更新时间：2020.1.31"
             echo
             red    "此脚本仅供交流学习使用，请勿使用此脚本行违法之事。网络非法外之地，行非法之事，必将接受法律制裁!!!!"
             tyblue "2019.11"
             ;;
         2)
-            clear
             tyblue "*************安装完成*************"
             tyblue "地址：${domain}"
             tyblue "端口：443"
             tyblue "ID：${v2id}"
             tyblue "额外ID：0"
-            tyblue "加密方式：任意(推荐：none)"
+            tyblue "加密方式：一般情况推荐none;若使用了cdn，推荐auto"
             tyblue "传输协议：ws"
             tyblue "伪装类型：none"
             tyblue "伪装域名：空"
-            tyblue "路径：/${path}"
+            tyblue "路径：${path}"
             tyblue "底层传输安全：tls"
             tyblue "**********************************"
             yellow "注意事项：如重新启动服务器，请执行/etc/nginx/sbin/nginx"
+            yellow "          或运行脚本，选择重启服务选项"
             echo
-            tyblue "脚本最后更新时间：2020.1.21"
+            tyblue "脚本最后更新时间：2020.1.31"
             echo
             red    "此脚本仅供交流学习使用，请勿使用此脚本行违法之事。网络非法外之地，行非法之事，必将接受法律制裁!!!!"
             tyblue "2019.11"
@@ -767,11 +710,84 @@ install_v2ray_ws_tls()
     esac
 }
 
+#配置v2ray_vmess
+config_v2ray_vmess()
+{
+cat > /etc/v2ray/config.json <<EOF
+{
+  "inbounds": [
+    {
+      "port": $port,
+      "listen": "127.0.0.1",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$v2id",
+            "level": 1,
+            "alterId": 0
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "$path"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+}
+
+#配置v2ray_socks
+config_v2ray_socks()
+{
+cat > /etc/v2ray/config.json <<EOF
+{
+  "inbounds": [
+    {
+      "port": $port,
+      "listen": "127.0.0.1",
+      "protocol": "socks",
+      "settings": {
+        "auth": "noauth",
+        "udp": false,
+        "userLevel": 999
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "$path"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+}
+
 #修改dns
 change_dns()
 {
-    red "ubuntu系统重启后会恢复原dns！！"
-    tyblue "此操作将修改dns服务器为1.1.1.1和1.0.0.1(cloudflare dns)"
+    red    "注意！！"
+    red    "1.部分云服务商(如阿里云)使用本地服务器作为软件包源，修改dns后需要换源！！"
+    red    "  如果听不懂，那么请在安装完v2ray+ws+tls后再修改dns，并且修改完后不要重新安装"
+    red    "2.Ubuntu系统重启后可能会恢复原dns"
+    tyblue "此操作将修改dns服务器为1.1.1.1和1.0.0.1(cloudflare公共dns)"
     if_change_dns="45"
     while [ "$if_change_dns" != "y" -a "$if_change_dns" != "n" ]
     do
@@ -789,43 +805,148 @@ change_dns()
     fi
 }
 
+
+#获取信息
+get_info()
+{
+    if ! grep -q "path" /etc/v2ray/config.json ; then
+        path=$(cat /dev/urandom | head -c 8 | md5sum | head -c 6)
+        path="/$path"
+    else
+        path=`grep path /etc/v2ray/config.json`
+        path=${path##*' '}
+        path=${path#*'"'}
+        path=${path%'"'*}
+    fi
+    port=`grep port /etc/v2ray/config.json`
+    port=${port##*' '}
+    port=${port%%,*}
+    if grep -q "id" /etc/v2ray/config.json ; then
+        v2id=`grep id /etc/v2ray/config.json`
+        v2id=${v2id##*' '}
+        v2id=${v2id#*'"'}
+        v2id=${v2id%'"'*}
+    fi
+}
+
+#使用socks作为底层协议
+turn_to_socks()
+{
+    tyblue "此操作将会把底层协议修改为socks(5)"
+    tyblue "关于这么做的意义，参考https://github.com/v2ray/discussion/issues/513"
+    if_turn_to_socks="45"
+    while [ "$if_turn_to_socks" != "y" -a "$if_turn_to_socks" != "n" ]
+    do
+        tyblue "是否要继续?(y/n)"
+        read if_turn_to_socks
+    done
+    if [ $if_turn_to_socks == "n" ]; then
+        exit 0;
+    fi
+    get_info
+    config_v2ray_socks
+    service v2ray restart
+    green  "配置完成！！！"
+    tyblue "将下面一段文字复制下来，保存到文本文件中"
+    tyblue "将“你的域名”四个字修改为你的其中一个域名(保留引号)，即原配置中“地址”一栏怎么填，这里就怎么填"
+    tyblue "并将文本文件重命名为config.json"
+    tyblue "然后在V2RayN/V2RayNG中，选择导入自定义配置，选择config.json"
+    yellow "******************以下是文本******************"
+cat <<EOF
+{
+  "log": {
+    "access": "",
+    "error": "",
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "listen": "127.0.0.1",
+      "port": 10808,
+      "protocol": "socks",
+      "sniffing": {
+        "enabled": true,
+        "destOverride": ["http", "tls"]
+      },
+      "settings": {
+        "auth": "noauth",
+        "userLevel": 10,
+        "udp": true
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "socks",
+      "settings": {
+        "servers": [
+          {
+            "address": "你的域名",
+            "level": 10,
+            "port": 443
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "wsSettings": {
+          "path": "$path"
+        }
+      },
+      "mux": {
+        "enabled": true,
+        "concurrency": 8
+      }
+    }
+  ]
+}
+EOF
+}
+
 #开始菜单
 start_menu()
 {
     stty erase '^H'
+    if [ "$EUID" != "0" ]; then
+        red "请用root用户运行此脚本！！"
+        exit 1
+    fi
     clear
-    tyblue "*****************************************************"
-    tyblue "v2ray  WebSocket(ws)+TLS(1.3)+Web  搭建脚本"
+    tyblue "************* V2Ray  WebSocket(ws)+TLS(1.3)+Web  搭建/管理脚本*************"
     tyblue "脚本特性："
     tyblue "1.集成安装bbr(2)加速"
-    tyblue "2.支持多种系统(ubuntu centos debian ...)"
+    tyblue "2.支持多种系统(Ubuntu Centos Debian ...)"
     tyblue "3.集成TLS配置多版本安装选项"
     tyblue "4.集成删除防火墙、阿里云盾功能"
     tyblue "5.使用nginx作为网站服务"
     tyblue "6.使用acme.sh自动申请域名证书"
-    tyblue "官网：https://github.com/kirin10000/v2ray-WebSocket-TLS-Web-setup-script"
-    tyblue "*****************************************************"
+    tyblue "官网：https://github.com/kirin10000/V2Ray-WebSocket-TLS-Web-setup-script"
+    tyblue "***************************************************************************"
     echo
-    tyblue "*****************************************************"
+    tyblue "***************************************************************************"
     red    "此脚本需要一个解析到本服务器的域名!!!!"
     yellow "此脚本需要一个解析到本服务器的域名!!!!"
     tyblue "此脚本需要一个解析到本服务器的域名!!!!"
     red    "全程建议不要使用小键盘"
     yellow "全程建议不要使用小键盘"
-    tyblue "推荐服务器系统使用ubuntu最新版"
-    tyblue "*****************************************************"
-    green  "1.安装v2ray-WebSocket(ws)+TLS(1.3)+Web(内含bbr安装选项)"
-    red    "2.删除v2ray-WebSocket(ws)+TLS(1.3)+Web"
-    tyblue "3.重启v2ray-WebSocket(ws)+TLS(1.3)+Web服务(对于玄学断连/掉速有奇效)"
-    tyblue "4.重置域名和TLS配置(会覆盖原有域名配置，配置过程中域名输错了造成v2ray无法启动可以用此选项修复)"
+    tyblue "推荐服务器系统使用Ubuntu最新版"
+    tyblue "***************************************************************************"
+    green  "1.安装V2Ray-WebSocket(ws)+TLS(1.3)+Web"
+    green  "  (内含bbr安装选项/支持覆盖安装、升级，如要升级，先下载最新脚本再安装)"
+    red    "2.删除V2Ray-WebSocket(ws)+TLS(1.3)+Web"
+    tyblue "3.重启/启动V2Ray-WebSocket(ws)+TLS(1.3)+Web服务(对于玄学断连/掉速有奇效)"
+    tyblue "4.重置域名和TLS配置"
+    tyblue "  (会覆盖原有域名配置，配置过程中域名输错了造成V2Ray无法启动可以用此选项修复)"
     tyblue "5.添加域名(不同域名可以有不同的TLS配置)"
-    tyblue "6.升级v2ray"
+    tyblue "6.使用socks(5)作为底层传输协议(beta)"
     tyblue "7.仅安装bbr(2)"
     tyblue "8.修改dns"
-    yellow "9.退出脚本"
+    tyblue "9.仅升级V2Ray"
+    yellow "10.退出脚本"
     echo
     menu="3345"
-    while [ "$menu" != "1" -a "$menu" != "2" -a "$menu" != "3" -a "$menu" != "4" -a "$menu" != "5" -a "$menu" != "6" -a "$menu" != "7" -a "$menu" != "8" -a "$menu" != "9" ]
+    while [ "$menu" != "1" -a "$menu" != "2" -a "$menu" != "3" -a "$menu" != "4" -a "$menu" != "5" -a "$menu" != "6" -a "$menu" != "7" -a "$menu" != "8" -a "$menu" != "9" -a "$menu" != "10" ]
     do
         read -p "您的选择是：" menu
     done
@@ -848,14 +969,8 @@ start_menu()
             readDomain
             readTlsConfig
             get_certs
-            port=`grep port /etc/v2ray/config.json`
-            port=${port##*' '}
-            port=${port%%,*}
-            path=`grep path /etc/v2ray/config.json`
-            path=${path#*/}
-            path=${path%'"'*}
+            get_info
             configtls
-            get_web
             /etc/nginx/sbin/nginx
             green "重置域名完成！！"
             case "$domainconfig" in
@@ -871,14 +986,8 @@ start_menu()
             readDomain
             readTlsConfig
             get_certs
-            port=`grep port /etc/v2ray/config.json`
-            port=${port##*' '}
-            port=${port%%,*}
-            path=`grep path /etc/v2ray/config.json`
-            path=${path#*/}
-            path=${path%'"'*}
+            get_info
             new_tls
-            get_web
             /etc/nginx/sbin/nginx
             green "添加域名完成！！"
             case "$domainconfig" in
@@ -891,10 +1000,7 @@ start_menu()
             esac
             ;;
         6)
-            if ! bash <(curl -L -s https://install.direct/go.sh) ; then
-                red    "你的服务器貌似没联网，或不支持ipv4，请检查网络连接"
-                yellow "v2ray更新失败"
-            fi
+            turn_to_socks
             ;;
         7)
             install_bbr
@@ -904,6 +1010,12 @@ start_menu()
             ;;
         8)
             change_dns
+            ;;
+        9)
+            if ! bash <(curl -L -s https://install.direct/go.sh) ; then
+                red    "你的服务器貌似没联网，或不支持ipv4，请检查网络连接"
+                yellow "v2ray更新失败"
+            fi
             ;;
     esac
 }
