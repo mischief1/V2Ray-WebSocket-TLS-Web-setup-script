170d357ab2d91de6986ec322884b9a88874130d7
kirin10000 <57820613+kirin10000@users.noreply.github.com>
2020-03-06 22:45:46 +0800

    Update v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh

--- v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
+++ v2ray-WebSocket(ws)+TLS(1.3)+Web-setup.sh
@@ -775,7 +775,7 @@ install_v2ray_ws_tls()
     yum install -y gperftools-devel libatomic_ops-devel pcre-devel zlib-devel libxslt-devel gd-devel perl-ExtUtils-Embed geoip-devel lksctp-tools-devel libxml2-devel gcc gcc-c++ wget unzip curl                   ##libxml2-devel非必须
     if cat /etc/issue | grep -qi "ubuntu" || cat /proc/version | grep -qi "ubuntu" ; then
         if version_ge $systemVersion 20.04 ; then
-            apt -y purge gcc g++ gcc-9 gcc-9-base gcc-8-base gcc-7-base g++-9 gcc-8 g++-8 gcc-7 g++-7
+            apt -y purge gcc g++ gcc-9 g++-9 gcc-8 g++-8 gcc-7 g++-7
             apt autopurge -y
             apt -y install gcc-10 g++-10
             ln -s /usr/bin/gcc-10 /usr/bin/gcc
